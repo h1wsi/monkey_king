8690e62d0bcb4dca857cd5906e01abe6bda83553
exception <iup5drep8.ne@gmail.com>
2023-12-07 19:56:12 +0300

    added a game window

--- game/main.py
+++ game/main.py
@@ -3,26 +3,35 @@ import os
 import random
 import pygame as pg
 
-import game.constants as c
-
+# вывод окна в центре экрана
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 pg.init()
 
 inf = pg.display.Info()
+
+# значения ширины и высоты текущего экрана
+SCREEN_WIDTH, SCREEN_HEIGHT = inf.current_w, inf.current_h
+# значения ширины и высоты игрового окна
+WINDOW_WIDTH, WINDOW_HEIGHT = SCREEN_WIDTH - 800, SCREEN_HEIGHT - 150
+
+FPS = 60
+
 pg.display.set_caption("Monkey King")
 timer = pg.time.Clock()
 
-screen = pg.display.set_mode([c.WINDOW_WIDTH, c.WINDOW_HEIGHT])
+screen = pg.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 
-section_width = c.WINDOW_WIDTH // 32
-section_height = c.WINDOW_HEIGHT // 32
+# определение высоты и ширины платформ, а также угол наклона
+section_width = WINDOW_WIDTH // 32
+section_height = WINDOW_HEIGHT // 32
 
 incline = section_height // 8
 
+# игровой loop
 run_game = True
-
 while run_game:
     screen.fill('black')
-    timer.tick(c.FPS)
+    timer.tick(FPS)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
