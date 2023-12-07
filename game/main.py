0e0d62ea5e053beb2366d0a4e1d4c88e946986d0
exception <iup5drep8.ne@gmail.com>
2023-12-07 22:34:00 +0300

    refresh the data and added platforms

--- game/main.py
+++ game/main.py
@@ -1,6 +1,5 @@
 
 import os
-import random
 import pygame as pg
 
 # вывод окна в центре экрана
@@ -16,23 +15,61 @@ WINDOW_WIDTH, WINDOW_HEIGHT = SCREEN_WIDTH - 800, SCREEN_HEIGHT - 150
 
 FPS = 60
 
-pg.display.set_caption("Monkey King")
 timer = pg.time.Clock()
-
 screen = pg.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 
-# определение высоты и ширины платформ, а также угол наклона
+# определение высоты и ширины секций платформ, а также угол наклона
 section_width = WINDOW_WIDTH // 32
 section_height = WINDOW_HEIGHT // 32
 
 incline = section_height // 8
 
+# класс для создания платформы
+class Platform():
+    def __init__(self, x_position, y_position, length):
+
+        self.x_position = x_position * section_width
+        self.y_position = y_position
+        self.length = length
+        self.top = self.draw()
+
+    def draw(self):
+        line_width = 7
+        platform_color = (225, 51, 129)
+
+        for i in range(self.length):  
+
+            # определение координат границ прямоугольника
+            bottom_coordinate = self.y_position + section_height 
+            left_coordinate = self.x_position + (section_width * i) 
+            right_coordinate = left_coordinate + section_width 
+            top_coordinate = self.y_position
+            # создание изображения платформы
+            pg.draw.rect(screen, platform_color, (left_coordinate, top_coordinate, right_coordinate - left_coordinate, bottom_coordinate - top_coordinate))
+            # определение линии, по которой будет передвигаться персонаж
+            top_line = pg.rect.Rect((self.x_position, self.y_position), (self.length * section_width, 2))
+        
+        return top_line
+
+# функция изображения игрового поля
+def draw_field():
+    platforms = [(5, 500, 5)]
+    ladders = []
+    platform_objs = []
+
+    for platform in platforms:
+        platform_objs.append(Platform(*platform))
+
+
 # игровой loop
 run_game = True
+
 while run_game:
     screen.fill('black')
     timer.tick(FPS)
 
+    draw_field()
+    
     for event in pg.event.get():
         if event.type == pg.QUIT:
 
